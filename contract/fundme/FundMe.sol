7004994566367144e773272d772a043c04ce556d
JasonCao1024 <1967675560@qq.com>
2025-03-16 23:31:06 +0800

    add lockTime

--- contract/fundme/FundMe.sol
+++ contract/fundme/FundMe.sol
@@ -12,26 +12,29 @@ contract FundMe {
 
     uint256 constant MINIMUN_VALUE = 10 * 10**18; //wei
 
+    // chainlink提供的dataFeed服务 获取美元价格
     AggregatorV3Interface internal dataFeed;
 
     uint256 constant TARGET = 20 * 10**18;
-
     address owner;
 
-    constructor() {
+    uint256 deploymentTimestamp;
+    uint256 lockTime;
+
+    constructor(uint256 _lockTime) {
         // Sepolia test
         dataFeed = AggregatorV3Interface(
             0x694AA1769357215DE4FAC081bf1f309aDC325306
         );
 
         owner = msg.sender;
+        deploymentTimestamp = block.timestamp;
+        lockTime = _lockTime;
     }
 
     function fund() external payable {
-        require(
-            convertEthToUsd(msg.value) >= MINIMUN_VALUE,
-            "You don't have enough money"
-        );
+        require(convertEthToUsd(msg.value) >= MINIMUN_VALUE,"You don't have enough money");
+        require(block.timestamp < deploymentTimestamp + lockTime,"window is closed");
         funderToAmount[msg.sender] = msg.value;
     }
 
@@ -59,44 +62,39 @@ contract FundMe {
         return (ethAmount * ethPrice) / (10**8);
     }
 
-    function getFund() external {
-        require(
-            convertEthToUsd(address(this).balance) >= TARGET,
-            "Target is not reached!"
-        );
-        require(
-            msg.sender == owner,
-            "this function can only be called by owner "
-        );
+    function getFund() external windowClose onlyOwner{
+        require(convertEthToUsd(address(this).balance) >= TARGET,"Target is not reached!");
+
         // transfer: transfer ETH and revert if tx failed
         // payable(msg.sender).transfer(address(this).balance);
         //send: transfer ETH and return false if failed
         // bool success = payable(msg.sender).send(address(this).balance);
         //call: transfer ETH with data retrun value of function and bool
         bool success;
-        (success, ) = payable(msg.sender).call{value: address(this).balance}(
-            ""
-        );
+        (success, ) = payable(msg.sender).call{value: address(this).balance}("");
     }
 
-    function transferOwnership(address newOwner) public {
-        require(
-            msg.sender == owner,
-            "this function can only be called by owner "
-        );
+    function transferOwnership(address newOwner) public onlyOwner {
         owner = newOwner;
     }
 
-    function reFund() external {
-        require(
-            convertEthToUsd(address(this).balance) < TARGET,
-            "Target is reached"
-        );
+    function reFund() external windowClose {
+        require(convertEthToUsd(address(this).balance) < TARGET,"Target is reached");
+
         require(funderToAmount[msg.sender] != 0, "there is no fund for you ");
         bool success;
-        (success, ) = payable(msg.sender).call{
-            value: funderToAmount[msg.sender]
-        }("");
+        (success, ) = payable(msg.sender).call{value: funderToAmount[msg.sender]}("");
         require(success, "transfer tx failed");
+        funderToAmount[msg.sender] = 0;
+    }
+
+    modifier windowClose() {
+        require(block.timestamp >= deploymentTimestamp + lockTime,"window is not closed");
+        _;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner,"this function can only be called by owner ");
+        _;
     }
 }
